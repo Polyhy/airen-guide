75f61199459d241cfe73335d6a9c571c58025a4e
Polyhy Gwon <polyhy@airensoft.com>
2016-01-18 14:44:16 +0900

    init restaurant card

--- libs/components/restaurant-card.jsx
+++ libs/components/restaurant-card.jsx
@@ -5,3 +5,58 @@ RestaurantElement = React.createClass({
 		return false;
 	}
 });
+
+RestaurantElement.Page1 = React.createClass({
+	propTypes: {
+		getRestaurantInfo: PropTypes.func.isRequired
+	},
+	getImageURL: function(){
+		var imageId = this.props.restaurant.images[0];
+		return RestaurantImages.findOne({_id: imageId}).url();
+	},
+	renderTableRow: function() {
+		var restaurantInfo = this.props.getRestaurantInfo(1, this.props.restaurant);
+		return restaurantInfo.map((i)=>{
+			return (
+					<tr>
+						<td>{i[0]}</td>
+						<td>{i[1]}</td>
+					</tr>
+			)
+		})
+	},
+	render: function(){
+		return (
+				<div>
+					<img src={this.getImageURL()} alt="" style={{width: "100%"}}/>
+					<table>
+						<tbody>
+							{this.renderTableRow()}
+						</tbody>
+					</table>
+				</div>
+		)
+	}
+});
+
+
+RestaurantElement.Page2 = React.createClass({
+	render: function(){
+		return false;
+	}
+});
+
+
+const {Page1, Page2} = RestaurantElement;
+RestaurantCard = React.createClass({
+	render: function(){
+		return (
+			<div className="restaurant-card" ref="card">
+				<p className="title restaurant-card--title">{this.props.restaurant.name}</p>
+				<div className="restaurant-card--info">
+					<Page1 restaurant={this.props.restaurant} getRestaurantInfo={this.props.getRestaurantInfo}/>
+				</div>
+			</div>
+		);
+	}
+});
--- libs/components/restaurants-list.jsx
+++ libs/components/restaurants-list.jsx
@@ -1,5 +1,44 @@
 RestaurantList = React.createClass({
+	subItems: [],
+	mixins: [ReactMeteorData],
+	getMeteorData: function(){
+		return {
+			restaurants: Restaurants.find({}).fetch()
+		}
+	},
+	componentWillMount: function(){
+		this.subItems.push(Meteor.subscribe('restaurants'));
+		this.subItems.push(Meteor.subscribe('restaurants-image'));
+	},
+	componentWillUnmount: function(){
+		for(var i=0; i<this.subItems.length; i++){
+			this.subItems[i].stop();
+		}
+	},
+	renderRestaurantItems: function(){
+		var getRestaurantInfo= function(page, restaurant){
+			if (page==1) return[
+					["대표메뉴", restaurant.menus.map((i)=>{return i.menu}).join(", ")],
+					["가격", restaurant.menus.reduce((m1, m2)=>{return m1+parseInt(m2.price)}, 0)],
+					["오픈~마감", restaurant.openTime+"  ~  "+restaurant.closeTime],
+					["아이렝스타", restaurant.rating]
+			];
+			else if (page==2) return [
+
+			];
+		};
+
+		return this.data.restaurants.map((restaurant)=>{
+			return (
+					<div className="col-xs-12 col-md-4 col-lg-3" key={restaurant._id}>
+						<RestaurantCard restaurant={restaurant} getRestaurantInfo={getRestaurantInfo}/>
+					</div>
+			)
+		});
+	},
 	render: function(){
-		return false;
+		return (
+				<div>{this.renderRestaurantItems()}</div>
+		)
 	}
 });
