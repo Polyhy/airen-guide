7b3e961f9174379ae1dc8778e99b95e0d7cac920
Polyhy Gwon <polyhy@airensoft.com>
2016-01-21 14:44:06 +0900

    Restaurant Detail

--- libs/components/restaurants-detail.jsx
+++ libs/components/restaurants-detail.jsx
@@ -39,6 +39,39 @@ RestaurantDetail = React.createClass({
 			)
 		});
 	},
+	renderTags: function(){
+		var tags = this.restaurant.tags? this.restaurant.tags.slice(): [];
+		var index = 0;
+		return tags.map(tag=><span key={"tag-"+index++}><i className="fa fa-hashtag"></i>{tag}</span>);
+	},
+	renderMenu: function(){
+		var i=0;
+		return this.restaurant.menus.map(m=>(
+				<tr className="menu-itme" key={i++}>
+					<td className="menu">{m.menu}</td>
+					<td className="price">{m.price}</td>
+				</tr>
+		));
+	},
+	renderStar: function(count){
+		return _.range(3).map(i=>i<count?
+				<span className="star y" key={"star-"+i}><i className="fa fa-star"/></span>:
+				<span className="star n" key={"star-"+i}><i className="star-o" className="fa fa-star-o"/></span>
+		);
+	},
+	renderRestDay: function(){
+		var i = 0;
+		var weeks = ["일", "월", "화", "수", "목", "금", "토"];
+		if (this.restaurant.closingType == "rest-date")
+			return this.restaurant.closingDays.map(d=>(
+				<p className="rest-day" key={i++}>매월 {d} 일</p>
+			));
+		else if (this.restaurant.closingType == "rest-week-day")
+			return this.restaurant.closingDays.map(d=>(
+				<p className="rest-day" key={i++}>매월 {d.week}주 {weeks[d.day]}요일</p>
+			));
+		else return <p className="rest-day">연중 무휴</p>
+	},
 	render: function(){
 		return (
 				<div id="restaurant-detail">
@@ -49,9 +82,44 @@ RestaurantDetail = React.createClass({
 					</h1>
 					<div className="head-images-container">
 						{this.renderHeaderImage(this.restaurant.images)}
-						<div></div>
+						<div className="tag-list">
+							<div>{this.renderTags()}</div>
+							<span style={{fontSize: "16px",fontWeight: "200",
+														position: "absolute",right: "0px",bottom: "18px",
+														textDecoration: "underline", color: "#bbb",cursor: "pointer"}}>
+								폐업/이전 신고
+							</span>
+						</div>
 					</div>
-					<div className="article article-1"></div>
+					<div className="article article-1">
+						<p className="title">대표 메뉴</p>
+						<div className="contents">
+							<table>
+								<tbody>{this.renderMenu()}</tbody>
+							</table>
+						</div>
+						<p className="title">아이렝 스타</p>
+						<div className="contents">{this.renderStar(this.restaurant.rating)}</div>
+						<p className="title">아이레너 코멘트</p>
+						<div className="contents">{this.restaurant.comment}</div>
+					</div>
+					<div className="map-wrapper">
+						<PolyhyComponent.Map width={"100%"} height={"100%"}
+																 lat={this.restaurant.latlng.lat}
+																 lng={this.restaurant.latlng.lng}
+																 zoom={18} marker={true}/>
+					</div>
+					<div className="article article-2">
+						<p className="title">주소</p>
+						<div className="contents">{this.restaurant.address}</div>
+						<p className="title">오픈 ~ 마감</p>
+						<div className="contents">{this.restaurant.openTime+" ~ "+this.restaurant.closeTime}</div>
+						<p className="title">휴무일</p>
+						<div className="contents">
+							{this.renderRestDay()}
+						</div>
+					</div>
+
 				</div>
 		)
 	}
