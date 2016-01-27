f6961e4bd82da2a775d9eab52b67e49e3fc64273
Polyhy Gwon <polyhy@airensoft.com>
2016-01-27 18:40:37 +0900

    Subscribe data

--- libs/components/todays-vote.jsx
+++ libs/components/todays-vote.jsx
@@ -1,6 +1,25 @@
 const {PropTypes} = React;
 
 TodaysVote = React.createClass({
+	subItems: [],
+	mixins: [ReactMeteorData],
+	getMeteorData: function(){
+		var teamId = Meteor.user().profile.teamId;
+		return {
+			//restaurants: Restaurants.find({}).fetch()
+			todaysRestaurants: Todays.findOne({teamId:teamId, status:1})
+		}
+	},
+	componentWillMount: function(){
+		this.subItems.push(Meteor.subscribe('restaurants'));
+		this.subItems.push(Meteor.subscribe('restaurants-image'));
+		this.subItems.push(Meteor.subscribe('today', Meteor.user().profile.teamId));
+	},
+	componentWillUnmount: function(){
+		for(var i=0; i<this.subItems.length; i++){
+			this.subItems[i].stop();
+		}
+	},
 	render: function(){
 		return false;
 	}
