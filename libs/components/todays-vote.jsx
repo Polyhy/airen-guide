d018fd2fbdb1b36c6106ec6aec51c934d1e9b644
Polyhy Gwon <polyhy@airensoft.com>
2016-01-27 20:39:25 +0900

    Add Todays vote view

--- libs/components/todays-vote.jsx
+++ libs/components/todays-vote.jsx
@@ -5,9 +5,11 @@ TodaysVote = React.createClass({
 	mixins: [ReactMeteorData],
 	getMeteorData: function(){
 		var teamId = Meteor.user().profile.teamId;
+		var todaysVote = Todays.findOne({teamId:teamId, status:1});
+		var restaurantsIds = todaysVote.restaurants.map(i=>i.restaurantsId);
 		return {
-			//restaurants: Restaurants.find({}).fetch()
-			todaysRestaurants: Todays.findOne({teamId:teamId, status:1})
+			restaurants: Restaurants.find({_id:{$in:restaurantsIds}}).fetch(),
+			todaysVote: todaysVote
 		}
 	},
 	componentWillMount: function(){
@@ -20,7 +22,38 @@ TodaysVote = React.createClass({
 			this.subItems[i].stop();
 		}
 	},
+	renderRestaurantItems: function() {
+		var getRestaurantInfo = function (page, restaurant) {
+			var voteRestaurant = this.state.todaysVote.restaurants.find(i=>i.restaurantsId==restaurant._id);
+			if (page == 1) return [
+				["대표메뉴", restaurant.menus.map((i)=> {
+					return i.menu
+				}).join(", ")],
+				["가격", String.fromCharCode(8361) + " " + restaurant.menus.reduce((m1, m2)=> {
+					return m1 + parseInt(m2.price)
+				}, 0) / restaurant.menus.length],
+				["오픈~마감", restaurant.openTime + "  ~  " + restaurant.closeTime],
+				["아이렝스타", _.range(3).map(i=><i
+						className={i<restaurant.rating?"fa fa-star":"fa fa-star-o"}
+						key={"star"+i}/>)]
+			];
+			else if (page == 2) return [
+				["주소", restaurant.address],
+				["인원", (voteRestaurant.maxMember-voteRestaurant.partyMember.length)+"/"+voteRestaurant.maxMember]
+			];
+		};
+
+		return this.data.restaurants.map((restaurant)=>{
+			return (
+					<div className="col-xs-12 col-sm-6 col-md-4 col-lg-3" key={restaurant._id}>
+						<RestaurantCard restaurant={restaurant} getRestaurantInfo={getRestaurantInfo} vote={true}/>
+					</div>
+			)
+		});
+	},
 	render: function(){
-		return false;
+		return (
+				<div>{this.renderRestaurantItems()}</div>
+		);
 	}
 });
